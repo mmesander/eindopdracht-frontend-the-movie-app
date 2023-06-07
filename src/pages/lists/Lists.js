a9a9dd5b113f53bddd8226a66c0c2e6853f41582
mmesander <mark_mesander@hotmail.com>
2023-06-07 12:01:18 +0200

    add title on pages

---
+++ src/pages/lists/Lists.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Lists() {
+    return (
+        <h1>This is the Lists page</h1>
+    )
+}
+
+export default Lists;
---
+++ src/pages/suggestion/Suggestion.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Suggestion() {
+    return (
+        <h1>This is the Suggestion page</h1>
+    )
+}
+
+export default Suggestion;
