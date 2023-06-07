d5758cf6183fb85bd2e68aac742cf471c66599b4
mmesander <mark_mesander@hotmail.com>
2023-06-07 13:10:51 +0200

    add logout function to logout button in nav bar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,9 +1,17 @@
-import React from "react";
-import logo from '../../assets/tma-logo.png'
+// Styles
 import './NavBar.css'
+
+// Assets
+import logo from '../../assets/tma-logo.png'
+
+//Functions
+import React, {useContext} from "react";
 import {NavLink} from "react-router-dom";
+import {AuthContext} from "../../context/AuthContext";
 
 function NavBar() {
+    const {logout} = useContext(AuthContext);
+
     return (
         <nav className="navbar-container">
             <NavLink to="/">
@@ -16,11 +24,36 @@ function NavBar() {
             </NavLink>
 
             <ul>
-                <li><NavLink to="/" className={({isActive})=> isActive ? 'active-nav-link' : 'default-nav-link'}>Home</NavLink></li>
-                <li><NavLink to="/zoeken" className={({isActive})=> isActive ? 'active-nav-link' : 'default-nav-link'}>Zoeken</NavLink></li>
-                <li><NavLink to="/suggestie" className={({isActive})=> isActive ? 'active-nav-link' : 'default-nav-link'}>Suggestie</NavLink></li>
-                <li><NavLink to="/lijsten" className={({isActive})=> isActive ? 'active-nav-link' : 'default-nav-link'}>Lijsten</NavLink></li>
-                <li><button type="button" className="nav-logout-button">Uitloggen</button></li>
+                <li><NavLink
+                    to="/"
+                    className={({isActive})=> isActive ? 'active-nav-link' : 'default-nav-link'}
+                >
+                    Home
+                </NavLink></li>
+                <li><NavLink
+                    to="/zoeken"
+                    className={({isActive})=> isActive ? 'active-nav-link' : 'default-nav-link'}
+                >
+                    Zoeken
+                </NavLink></li>
+                <li><NavLink
+                    to="/suggestie"
+                    className={({isActive})=> isActive ? 'active-nav-link' : 'default-nav-link'}
+                >
+                    Suggestie
+                </NavLink></li>
+                <li><NavLink
+                    to="/lijsten"
+                    className={({isActive})=> isActive ? 'active-nav-link' : 'default-nav-link'}
+                >
+                    Lijsten
+                </NavLink></li>
+                <li><button
+                    type="button"
+                    className="nav-logout-button"
+                    onClick={logout}
+                >
+                    Uitloggen</button></li>
             </ul>
         </nav>
     )
