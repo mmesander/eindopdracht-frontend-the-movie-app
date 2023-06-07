e1ff278c82fe8061a3923cf31c40034cf83dffc0
mmesander <mark_mesander@hotmail.com>
2023-06-07 12:02:07 +0200

    import pages on app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 // Styles
 import './App.css';
 
+// Pages
+import Home from "./pages/home/Home";
+import Search from "./pages/search/Search";
+import Suggestion from "./pages/suggestion/Suggestion";
+import Lists from "./pages/lists/Lists";
+
 // Components
 import NavBar from "./components/NavBar";
 
