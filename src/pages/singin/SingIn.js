b72e095d0fe6d9d5be36740adb0f26541404351b
mmesander <mark_mesander@hotmail.com>
2023-06-07 12:45:27 +0200

    create login page content

---
+++ src/pages/singin/SingIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SingIn() {
+    return (
+        <>
+            <h1>This is the singin page</h1>
+            <button type="button">Inloggen</button>
+        </>
+    )
+}
+
+export default SingIn;
