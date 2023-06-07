5726ae541af61bee8746375c2c7344f84a033035
mmesander <mark_mesander@hotmail.com>
2023-06-07 13:18:19 +0200

    add login function to login button

--- src/pages/singin/SingIn.js
+++ src/pages/singin/SingIn.js
@@ -1,12 +1,21 @@
-import React from "react";
+// Functions
+import React, {useContext} from "react";
+
+// Context
+import authContext, {AuthContext} from "../../context/AuthContext";
 
 function SingIn() {
-
+    const {login} = useContext(AuthContext);
 
     return (
         <>
             <h1>This is the singin page</h1>
-            <button type="button">Inloggen</button>
+            <button
+                type="button"
+                onClick={login}
+            >
+                Inloggen
+            </button>
         </>
     )
 }
