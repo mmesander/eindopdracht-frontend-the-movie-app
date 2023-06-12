ac193c16da81dc054cc9fb3856c972924b475c0e
mmesander <mark_mesander@hotmail.com>
2023-06-12 17:39:34 +0200

    import password context

--- src/context/PasswordCheckContext.js
+++ src/context/PasswordCheckContext.js
@@ -1,10 +1,12 @@
-import React, {createContext, useState} from "react";
+import React, {createContext, useContext, useState} from "react";
+import {PasswordContext} from "./PasswordContext";
 
 export const PasswordCheckContext = createContext("");
 
 function PasswordCheckContextProvider({children}) {
     const [passwordCheck, setPasswordCheck] = useState();
     const [passwordCheckError, setPasswordCheckError] = useState();
+    const {password} = useContext(PasswordContext);
 
     const data = {
         passwordCheck: passwordCheck,
