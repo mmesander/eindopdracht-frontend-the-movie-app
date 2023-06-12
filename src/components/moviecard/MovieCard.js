540081646a4b02075fb9ef9c62cf7e4e35fa5dcd
mmesander <mark_mesander@hotmail.com>
2023-06-12 20:09:48 +0200

    import and add moviecard to home.js

--- src/components/moviecard/MovieCard.js
+++ src/components/moviecard/MovieCard.js
@@ -7,7 +7,9 @@ import React from "react";
 function MovieCard() {
     return (
         <div className="moviecard-container">
-
+            <h1>Hier komt foto</h1>
+            <h3>Title</h3>
+            <h4>Rating</h4>
         </div>
     )
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,12 +4,15 @@ import './Home.css'
 // Functions
 import React from "react";
 
+// Components
+import MovieCard from "../../components/moviecard/MovieCard";
+
 function Home() {
     return (
         <>
             <div className="home-outer-container">
                 <div className="home-inner-container">
-                    <h1>This is the home page inner container</h1>
+                    <MovieCard/>
                 </div>
             </div>
         </>
