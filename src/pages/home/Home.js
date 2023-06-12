0ddcab9ccfa9fe2654ccb44e9008a1c91a8298ae
mmesander <mark_mesander@hotmail.com>
2023-06-12 20:47:46 +0200

    add setup and styling home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -11,12 +11,16 @@ function Home() {
     return (
         <>
             <div className="home-outer-container">
+                <h1 className="movies-title">Trending Movies</h1>
                 <div className="home-inner-container">
                     <MovieCard/>
                     <MovieCard/>
                     <MovieCard/>
                     <MovieCard/>
                     <MovieCard/>
+                </div>
+                <h1 className="series-title">Trending Series</h1>
+                <div className="home-inner-container">
                     <MovieCard/>
                     <MovieCard/>
                     <MovieCard/>
