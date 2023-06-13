72b9bdc1fbc7b7ef23fa60bc80c439a561fefd74
mmesander <mark_mesander@hotmail.com>
2023-06-14 01:10:42 +0200

    add map fetchSeries function and 2 map functions for homepage

--- src/components/moviecard/MovieCard.js
+++ src/components/moviecard/MovieCard.js
@@ -4,12 +4,14 @@ import './MovieCard.css'
 // Functions
 import React from "react";
 
-function MovieCard() {
+function MovieCard({title, image, rating}) {
+    const roundedRating = Math.round(rating * 10) / 10
+
     return (
         <div className="moviecard-container">
-            <h2>Hier komt foto</h2>
-            <h3>Title</h3>
-            <h4>Rating</h4>
+            <img src={`https://image.tmdb.org/t/p/w500${image}`} alt={title}/>
+            <h3>{title}</h3>
+            <h4>Rating: {roundedRating}</h4>
         </div>
     )
 }
