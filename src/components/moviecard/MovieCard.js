eebb62a987d9500991878c8fc9fcba7371fd8a98
mmesander <mark_mesander@hotmail.com>
2023-06-14 12:40:53 +0200

    add icons and basic styling to icons

--- src/components/moviecard/MovieCard.js
+++ src/components/moviecard/MovieCard.js
@@ -1,6 +1,11 @@
 // Styling
 import './MovieCard.css'
 
+// Assets
+import favorite from '../../assets/heart-straight-fill.svg'
+import watchlist from '../../assets/eye-fill.svg'
+import watched from '../../assets/check-fat-fill.svg'
+
 // Functions
 import React from "react";
 
@@ -9,9 +14,31 @@ function MovieCard({title, image, rating}) {
 
     return (
         <div className="moviecard-container">
-            <img src={`https://image.tmdb.org/t/p/w500${image}`} alt={title}/>
-            <h3>{title}</h3>
-            <h4>Rating: {roundedRating}</h4>
+            <section className="moviecard-header-section">
+                <img src={`https://image.tmdb.org/t/p/w500${image}`} alt={title}/>
+                <h3>{title}</h3>
+                <h4>Rating: {roundedRating}</h4>
+            </section>
+            <div className="icons-container">
+                <button
+                    type="button"
+                    // className={({isActive}) => isActive ? 'active-favourite-icon' : 'default-icon'}
+                >
+                    <img src={favorite} alt="favorite-icon"/>
+                </button>
+                <button
+                    type="button"
+                    // className={({isActive}) => isActive ? 'active-watchlist-icon' : 'default-icon'}
+                >
+                    <img src={watchlist} alt="watchlist-icon"/>
+                </button>
+                <button
+                    type="button"
+                    // className={({isActive}) => isActive ? 'active-watched-icon' : 'default-icon'}
+                >
+                    <img src={watched} alt="watched-icon"/>
+                </button>
+            </div>
         </div>
     )
 }
