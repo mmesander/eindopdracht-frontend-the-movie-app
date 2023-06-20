9cb24b39127d2d037660e36d1bffa19bc7c3aecb
mmesander <mark_mesander@hotmail.com>
2023-06-20 15:11:43 +0200

    add img and p element in moodcontainer

--- src/components/moodcontainer/MoodContainer.js
+++ src/components/moodcontainer/MoodContainer.js
@@ -4,11 +4,12 @@ import './MoodContainer.css'
 // Functions
 import React from "react";
 
-function MoodContainer({mood, image, endpoint}) {
-    return(
+function MoodContainer({mood, image, imageDescription}) {
+    return (
         <>
             <div className="mood-outer-container">
-
+                <img src={image} alt={imageDescription}/>
+                <p className="mood-description">{mood}</p>
             </div>
         </>
     )
