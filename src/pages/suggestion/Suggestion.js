8ceb3d74692a0082f38054b4993da5efa4493113
mmesander <mark_mesander@hotmail.com>
2023-06-20 19:57:22 +0200

    add basic async function

--- src/pages/suggestion/Suggestion.js
+++ src/pages/suggestion/Suggestion.js
@@ -13,22 +13,43 @@ import drama from '../../assets/images/mood-sad.jpg'
 
 // Components
 import MoodContainer from "../../components/moodcontainer/MoodContainer";
+import axios from "axios";
 
 function Suggestion() {
     const [active, setActive] = useState(false);
-    console.log(active)
+    const [movies, setMovies] = useState({});
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+        }
+    };
+
+    async function fetchSpecificMovies() {
+        try {
+            const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=35`, options);
+            console.log(response.data)
+            setActive(true);
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+
 
     return (
         <>
             <div className="suggestion-outer-container">
-                {!active && <section className="suggestion-inner-container">
+                {!active && <section className="suggestion-switch-container">
                     <h1 className="suggestion-title">Heb jij zin om: </h1>
                     <div className="suggestion-mood-container">
                         <MoodContainer
                             mood="van de bank te rollen van het lachen"
                             image={comedy}
                             imageDescription="mood image for comedy movies"
-                            onClick={() => setActive(true)}
+                            onClick={fetchSpecificMovies}
                         />
                         <MoodContainer
                             mood="op het puntje van de bank te zitten"
@@ -56,7 +77,7 @@ function Suggestion() {
                         />
                     </div>
                 </section>}
-                {active && <section>
+                {active && <section className="suggestion-switch-container">
                     <button
                         className="button-to-overview"
                         type="button"
@@ -64,6 +85,10 @@ function Suggestion() {
                     >
                         Terug naar overzicht
                     </button>
+                    <h1 className="suggestion-title">Je hebt gekozen voor iets. </h1>
+                    <div className="suggestion-inner-container">
+
+                    </div>
                 </section>}
             </div>
         </>
