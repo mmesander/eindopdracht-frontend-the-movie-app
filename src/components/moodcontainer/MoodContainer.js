4e6f0c01fb509ae2c17147f7af2d573b0746f57d
mmesander <mark_mesander@hotmail.com>
2023-06-20 14:43:51 +0200

    add moodcontainer

---
+++ src/components/moodcontainer/MoodContainer.js
@@ -0,0 +1,14 @@
+// Functions
+import React from "react";
+
+function MoodContainer({mood, image, endpoint}) {
+    return(
+        <>
+            <div className="mood-outer-container">
+
+            </div>
+        </>
+    )
+}
+
+export default MoodContainer;
