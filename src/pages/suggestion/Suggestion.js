f13ab265e914ea66e4f9b7c842a877187860850f
mmesander <mark_mesander@hotmail.com>
2023-06-20 20:51:24 +0200

    formatted suggestion.js

--- src/pages/suggestion/Suggestion.js
+++ src/pages/suggestion/Suggestion.js
@@ -42,7 +42,6 @@ function Suggestion() {
     }
 
 
-
     return (
         <>
             <div className="suggestion-outer-container">
@@ -53,31 +52,41 @@ function Suggestion() {
                             mood="van de bank te rollen van het lachen"
                             image={comedy}
                             imageDescription="mood image for comedy movies"
-                            onClick={() => {fetchSpecificMovies(35, "van de bank te rollen van het lachen")}}
+                            onClick={() => {
+                                fetchSpecificMovies(35, "van de bank te rollen van het lachen")
+                            }}
                         />
                         <MoodContainer
                             mood="op het puntje van je stoel te zitten"
                             image={adventure}
                             imageDescription="mood image for adventure movies"
-                            onClick={() => {fetchSpecificMovies('80%7C28%7C53', "op het puntje van je stoel te zitten")}}
+                            onClick={() => {
+                                fetchSpecificMovies('80%7C28%7C53', "op het puntje van je stoel te zitten")
+                            }}
                         />
                         <MoodContainer
                             mood="je af en toe te moeten verstoppen achter een dekentje"
                             image={horror}
                             imageDescription="mood image for horror movies"
-                            onClick={() => {fetchSpecificMovies(27, "je af en toe te moeten verstoppen achter een dekentje")}}
+                            onClick={() => {
+                                fetchSpecificMovies(27, "je af en toe te moeten verstoppen achter een dekentje")
+                            }}
                         />
                         <MoodContainer
                             mood="in een andere wereld te belanden"
                             image={otherworldly}
                             imageDescription="mood image for otherworldly movies"
-                            onClick={() => {fetchSpecificMovies('14%7C878&without_genres=27', "in een andere wereld te belanden")}}
+                            onClick={() => {
+                                fetchSpecificMovies('14%7C878&without_genres=27', "in een andere wereld te belanden")
+                            }}
                         />
                         <MoodContainer
                             mood="met een doos tissues op de bank te zitten"
                             image={drama}
                             imageDescription="mood image for sad movies"
-                            onClick={() => {fetchSpecificMovies('18%7C10749&without_genres=28', "met een doos tissues op de bank te zitten")}}
+                            onClick={() => {
+                                fetchSpecificMovies('18%7C10749&without_genres=28', "met een doos tissues op de bank te zitten")
+                            }}
                         />
                     </div>
                 </section>}
@@ -92,7 +101,8 @@ function Suggestion() {
                     <h1 className="suggestion-title">{`Je hebt gekozen om ${title}`}</h1>
                     <div className="suggestion-inner-container">
                         {Object.keys(movies).length > 0 && movies.map((movie) => {
-                            return <MovieCard key={movie.id} title={movie.name} image={movie.poster_path} rating={movie.vote_average}/>
+                            return <MovieCard key={movie.id} title={movie.name} image={movie.poster_path}
+                                              rating={movie.vote_average}/>
                         })}
                     </div>
                 </section>}
