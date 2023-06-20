c803d8bacb577573609b5e0dd8f91e26769a3313
mmesander <mark_mesander@hotmail.com>
2023-06-20 18:39:09 +0200

    change link to button

--- src/components/moodcontainer/MoodContainer.js
+++ src/components/moodcontainer/MoodContainer.js
@@ -3,17 +3,25 @@ import './MoodContainer.css'
 
 // Functions
 import React from "react";
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
 
-function MoodContainer({mood, image, imageDescription}) {
+function MoodContainer({mood, image, imageDescription, onClick}) {
+
+
     return (
         <>
-            <Link to="/" className="mood-link">
+            {/*<Link to="/" className="mood-link">*/}
+            {/*    <div className="mood-outer-container">*/}
+            {/*        <img src={image} alt={imageDescription}/>*/}
+            {/*        <h4 className="mood-description">{mood}</h4>*/}
+            {/*    </div>*/}
+            {/*</Link>*/}
+            <button type="radio" className="mood-button">
                 <div className="mood-outer-container">
                     <img src={image} alt={imageDescription}/>
                     <h4 className="mood-description">{mood}</h4>
                 </div>
-            </Link>
+            </button>
         </>
     )
 }
