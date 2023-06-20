f3f7bffee48ea85b4429ff22409cde4e91edc4d8
mmesander <mark_mesander@hotmail.com>
2023-06-20 15:45:39 +0200

    add button element

--- src/components/moodcontainer/MoodContainer.js
+++ src/components/moodcontainer/MoodContainer.js
@@ -3,14 +3,22 @@ import './MoodContainer.css'
 
 // Functions
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 function MoodContainer({mood, image, imageDescription}) {
+    const navigate = useNavigate();
+
     return (
         <>
-            <div className="mood-outer-container">
-                <img src={image} alt={imageDescription}/>
-                <h4 className="mood-description">{mood}</h4>
-            </div>
+            <button
+            type="button"
+            onClick={() => navigate("/")}
+            >
+                <div className="mood-outer-container">
+                    <img src={image} alt={imageDescription}/>
+                    <h4 className="mood-description">{mood}</h4>
+                </div>
+            </button>
         </>
     )
 }
