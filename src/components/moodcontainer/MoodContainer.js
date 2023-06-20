b26884f1c991f8a87ade594d5704944609e448a9
mmesander <mark_mesander@hotmail.com>
2023-06-20 16:47:50 +0200

    add styling to moodcontainer

--- src/components/moodcontainer/MoodContainer.js
+++ src/components/moodcontainer/MoodContainer.js
@@ -3,22 +3,17 @@ import './MoodContainer.css'
 
 // Functions
 import React from "react";
-import {useNavigate} from "react-router-dom";
+import {Link} from 'react-router-dom';
 
 function MoodContainer({mood, image, imageDescription}) {
-    const navigate = useNavigate();
-
     return (
         <>
-            <button
-            type="button"
-            onClick={() => navigate("/")}
-            >
+            <Link to="/" className="mood-link">
                 <div className="mood-outer-container">
                     <img src={image} alt={imageDescription}/>
                     <h4 className="mood-description">{mood}</h4>
                 </div>
-            </button>
+            </Link>
         </>
     )
 }
