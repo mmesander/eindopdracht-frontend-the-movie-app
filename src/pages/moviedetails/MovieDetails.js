bccfb8352dd0af74cafc03e240e93d11c6264a90
mmesander <mark_mesander@hotmail.com>
2023-06-21 12:25:44 +0200

    add\ new styling to moviecard

--- src/pages/moviedetails/MovieDetails.js
+++ src/pages/moviedetails/MovieDetails.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import './MovieDetails.css'
 import {Link, useParams} from "react-router-dom";
+import axios from "axios";
 
 function MovieDetails() {
     const {movieId} = useParams();
