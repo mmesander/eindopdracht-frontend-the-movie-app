9831f1042a8aa7e70e9f1adbe1366e7714f09f75
mmesander <mark_mesander@hotmail.com>
2023-06-27 13:39:47 +0200

    update footer and styling lists page

--- src/pages/lists/Lists.js
+++ src/pages/lists/Lists.js
@@ -1,9 +1,14 @@
 // Functions
 import React from "react";
 
+// Styles
+import './Lists.css'
+
 function Lists() {
     return (
-        <h1>This is the Lists page</h1>
+        <div className="page-outer-container">
+            <h1>This is the Lists page</h1>
+        </div>
     )
 }
 
