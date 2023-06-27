8382057298bf544ef83a8df47459702995c6dd49
mmesander <mark_mesander@hotmail.com>
2023-06-27 11:59:07 +0200

    create helperfunction format date

---
+++ src/helpers/formatDate.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function formatDate(date) {
+    const months = [
+        "januari", "februari", "maart", "april", "mei", "juni",
+        "juli", "augustus", "september", "oktober", "november", "december"
+    ];
+
+    const parts = date.split("-");
+    const day = parseInt(parts[2]);
+    const month = months[parseInt(parts[1]) - 1];
+    const year = parseInt(parts[0])
+
+    return `${day} ${month} ${year}`
+}
+
+export default formatDate;
