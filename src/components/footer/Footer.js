7fd51846e7f27fe3d8b92d1c538ec198d5120c0b
mmesander <mark_mesander@hotmail.com>
2023-06-27 13:01:12 +0200

    create footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+// Functions
+import React from "react";
+
+// Styles
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="footer-outer-container">
+            <h4>Deze applicatie is ontwikkeld door Mark Mesander</h4>
+        </div>
+    )
+}
