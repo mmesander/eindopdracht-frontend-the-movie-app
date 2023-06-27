45bd1bdd2be501f66d868e5360b04ba91375c8a5
mmesander <mark_mesander@hotmail.com>
2023-06-27 14:19:36 +0200

    add state in listcontext

--- src/context/ListsContext.js
+++ src/context/ListsContext.js
@@ -1,10 +1,21 @@
 // Functions
-import React, {createContext} from "react";
+import React, {createContext, useState} from "react";
 
 export const ListsContext = createContext(null);
 
 function ListsContextProvider({children}) {
+    const [favorite, setFavorite] = useState(false);
+    const [watchlist, setWatchlist] = useState(false);
+    const [watched, setWatched] = useState(false);
 
+    const data = {
+        favorite: favorite,
+        setFavorite: setFavorite,
+        watchlist: watchlist,
+        setWatchlist: setWatchlist,
+        watched: watched,
+        setWatched: setWatched,
+    }
 
     return (
         <ListsContext.Provider value={data}>
