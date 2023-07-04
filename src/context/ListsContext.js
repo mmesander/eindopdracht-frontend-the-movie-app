68977dd16f457f242c19a8ff6e9d054508e48928
mmesander <mark_mesander@hotmail.com>
2023-07-04 13:41:34 +0200

    add separate array's for movies and series in context

--- src/context/ListsContext.js
+++ src/context/ListsContext.js
@@ -5,19 +5,22 @@ export const ListsContext = createContext(null);
 
 function ListsContextProvider({children}) {
     const [listItem, setListItem] = useState({
-        favorite: [],
-        watchlist: [],
-        watched: [],
+        favoriteMovies: [],
+        watchlistMovies: [],
+        watchedMovies: [],
+        favoriteSeries: [],
+        watchlistSeries: [],
+        watchedSeries: [],
     });
 
-    useEffect(() => {
-        if (!localStorage.getItem("movies")) {
-            localStorage.setItem('movies', JSON.stringify(listItem))
-        }
-        if (!localStorage.getItem("series")) {
-            localStorage.setItem('series', JSON.stringify(listItem))
-        }
-    }, []);
+    // useEffect(() => {
+    //     if (!localStorage.getItem("movies")) {
+    //         localStorage.setItem('movies', JSON.stringify(listItem))
+    //     }
+    //     if (!localStorage.getItem("series")) {
+    //         localStorage.setItem('series', JSON.stringify(listItem))
+    //     }
+    // }, []);
 
     const data = {
         listItem: listItem,
