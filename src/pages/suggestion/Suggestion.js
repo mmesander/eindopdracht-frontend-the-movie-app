10564db51b979e4e635239377b63cfe34c5337e2
mmesander <mark_mesander@hotmail.com>
2023-07-04 18:03:42 +0200

    remove .switch-container from suggestion.js

--- src/pages/suggestion/Suggestion.js
+++ src/pages/suggestion/Suggestion.js
@@ -27,53 +27,51 @@ function Suggestion() {
     return (
         <>
             <div className="page-outer-container">
-                <section className="suggestion-switch-container">
-                    <h1 className="suggestion-title">Heb jij zin om: </h1>
-                    {/*<div className="loading-error-section">*/}
-                    {/*    {loading && <h3 className="loading-message">Loading... </h3>}*/}
-                    {/*    {error && <h3 className="error-message">Error: Could not fetch data!</h3>}*/}
-                    {/*</div>*/}
-                    <div className="suggestion-mood-container">
-                        <MoodContainer
-                            mood="van de bank te rollen van het lachen"
-                            image={comedy}
-                            imageDescription="mood image for comedy movies"
-                            onClick={() => clickHandler( "35", "van de bank te rollen van het lachen", "1")}
-                        />
-                        <MoodContainer
-                            mood="op het puntje van je stoel te zitten"
-                            image={adventure}
-                            imageDescription="mood image for adventure movies"
-                            onClick={() => {
-                                clickHandler("80%7C28%7C53", "op het puntje van je stoel te zitten", "2")
-                            }}
-                        />
-                        <MoodContainer
-                            mood="je af en toe te moeten verstoppen achter een dekentje"
-                            image={horror}
-                            imageDescription="mood image for horror movies"
-                            onClick={() => {
-                                clickHandler("27", "je af en toe te moeten verstoppen achter een dekentje", "3")
-                            }}
-                        />
-                        <MoodContainer
-                            mood="in een andere wereld te belanden"
-                            image={otherworldly}
-                            imageDescription="mood image for otherworldly movies"
-                            onClick={() => {
-                                clickHandler("14%7C878&without_genres=27", "in een andere wereld te belanden", "4")
-                            }}
-                        />
-                        <MoodContainer
-                            mood="met een doos tissues op de bank te zitten"
-                            image={drama}
-                            imageDescription="mood image for sad movies"
-                            onClick={() => {
-                                clickHandler("18%7C10749&without_genres=28", "met een doos tissues op de bank te zitten", "5")
-                            }}
-                        />
-                    </div>
-                </section>
+                <h1 className="suggestion-title">Heb jij zin om: </h1>
+                {/*<div className="loading-error-section">*/}
+                {/*    {loading && <h3 className="loading-message">Loading... </h3>}*/}
+                {/*    {error && <h3 className="error-message">Error: Could not fetch data!</h3>}*/}
+                {/*</div>*/}
+                <div className="suggestion-mood-container">
+                    <MoodContainer
+                        mood="van de bank te rollen van het lachen"
+                        image={comedy}
+                        imageDescription="mood image for comedy movies"
+                        onClick={() => clickHandler("35", "van de bank te rollen van het lachen", "1")}
+                    />
+                    <MoodContainer
+                        mood="op het puntje van je stoel te zitten"
+                        image={adventure}
+                        imageDescription="mood image for adventure movies"
+                        onClick={() => {
+                            clickHandler("80%7C28%7C53", "op het puntje van je stoel te zitten", "2")
+                        }}
+                    />
+                    <MoodContainer
+                        mood="je af en toe te moeten verstoppen achter een dekentje"
+                        image={horror}
+                        imageDescription="mood image for horror movies"
+                        onClick={() => {
+                            clickHandler("27", "je af en toe te moeten verstoppen achter een dekentje", "3")
+                        }}
+                    />
+                    <MoodContainer
+                        mood="in een andere wereld te belanden"
+                        image={otherworldly}
+                        imageDescription="mood image for otherworldly movies"
+                        onClick={() => {
+                            clickHandler("14%7C878&without_genres=27", "in een andere wereld te belanden", "4")
+                        }}
+                    />
+                    <MoodContainer
+                        mood="met een doos tissues op de bank te zitten"
+                        image={drama}
+                        imageDescription="mood image for sad movies"
+                        onClick={() => {
+                            clickHandler("18%7C10749&without_genres=28", "met een doos tissues op de bank te zitten", "5")
+                        }}
+                    />
+                </div>
             </div>
         </>
     )
