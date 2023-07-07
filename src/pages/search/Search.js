b9c6c88a0ea1df42a4668b763e09a14fad2e4594
mmesander <mark_mesander@hotmail.com>
2023-07-07 20:37:47 +0200

    update map function on filter search page

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -597,25 +597,26 @@ function Search() {
                         />
                     </div>}
                     <div className="filter-search-results-inner-container">
-                        {Object.keys(filterSearchResults).length > 0 && isMovie && filtersActive && filterSearchResults.map((movie) => {
-                            return <MovieCard
-                                key={movie.id}
-                                title={movie.title}
-                                image={movie.poster_path}
-                                rating={movie.vote_average}
-                                id={movie.id}
-                                tv={false}
-                            />
-                        })}
-                        {Object.keys(filterSearchResults).length > 0 && !isMovie && filtersActive && filterSearchResults.map((series) => {
-                            return <MovieCard
-                                key={series.id}
-                                title={series.name}
-                                image={series.poster_path}
-                                rating={series.vote_average}
-                                id={series.id}
-                                tv={true}
-                            />
+                        {Object.keys(filterSearchResults).length > 0 && filtersActive && filterSearchResults.map((search) => {
+                            if (!isMovie && search.name && search.poster_path && search.vote_average) {
+                                return <MovieCard
+                                    key={search.id}
+                                    name={search.name}
+                                    image={search.poster_path}
+                                    rating={search.vote_average}
+                                    id={search.id}
+                                    tv={true}
+                                />
+                            } else if (isMovie && search.title && search.poster_path && search.vote_average) {
+                                return <MovieCard
+                                    key={search.id}
+                                    title={search.title}
+                                    image={search.poster_path}
+                                    rating={search.vote_average}
+                                    id={search.id}
+                                    tv={false}
+                                />
+                            }
                         })}
                     </div>
                 </div>
