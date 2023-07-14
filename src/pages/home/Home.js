7cbaa2e37fa50e31cc5347119afc71e84ff19707
mmesander <mark_mesander@hotmail.com>
2023-07-14 18:30:25 +0200

    update handleClickMovies and handleClickSeries functions

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -78,12 +78,14 @@ function Home() {
         setMoreMovies(!moreMovies);
         setMoreSeries(false);
         setMoviePage(1);
+        setSeriesPage(1);
     }
 
     function handleClickSeries() {
         setMoreSeries(!moreSeries);
         setMoreMovies(false);
         setSeriesPage(1);
+        setMoviePage(1);
     }
 
     return (
